63715952ba2a09fcf78af147b07dc02554aeacac
tjteachey <thomasjteachey@gmail.com>
2025-09-29 19:37:53 -0400

    whoops

--- spBarracksMakeEverything.sql
+++ spBarracksMakeEverything.sql
@@ -1,8 +1,7 @@
 CREATE DEFINER=`brokilodeluxe`@`%` PROCEDURE `spBarracksMakeEverything`()
 BEGIN
 	call spBarracksResetCreatures;
-    call spBarracks_Backport_CMaNGOS_Creatures(0, 12340);
-    call spBarracks_Backport_Classic_Quests_OldWorld(12340);
+	call spBarracksResetQuests;
     call spBarracksMakeTrainers;
     call spBarracksMakeItems;
 END
